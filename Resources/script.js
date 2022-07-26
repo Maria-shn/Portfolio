e219c3a357a348923404091ff958d1448521e1ed
Мария <maria@MacBook-Air-Maria.local>
2022-07-26 07:18:20 +0200

    temporarily commented out functions

--- Resources/script.js
+++ Resources/script.js
@@ -2,7 +2,7 @@ const menubars =document.getElementById("menubars");
 const menu = document.getElementById("menu");
 
 
-let appear = function() {
+/*let appear = function() {
     menu.style.borderRight = "white thin";
     menu.style.transform = "translate(195px)";
 };
@@ -11,7 +11,7 @@ const disappear = function() {
     menubars.style.borderRight = "none";
     menubars.style.transform = "translate(-195px)";
     appear = false;
-};
+}; */
 
 menubars.addEventListener("touch", function() {
     menu.style.borderRight = "white thin";
